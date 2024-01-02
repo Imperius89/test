04cba1c8967a0603168ada20020d863c9c51e65e
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2024-01-02 16:12:50 -0500

    enero 02/01/2023

--- #exponente.py
+++
@@ -1,9 +0,0 @@
-#exponente
-
-n=int(input("Ingrese el numero a elevar a la 5"))
-i=0
-m=1
-while (i<5):
-    m=n*m
-    i=i+1
-    print(m)
--- borrador.py
+++
@@ -1,4 +0,0 @@
-var=100.2
-type(var)
-var2=True
-type(var2)
